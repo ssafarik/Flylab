b95e71c37112fd621fdc66d071211a55e6f614ff
Steve Safarik <ssafarik@uw.edu>
2012-02-28 15:30:48 -0800

    Removed & moved some old dirs & files.

--- ros/data/save_data/msg_gen/lisp/_package_VideoInfo.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package save_data-msg)
-(cl:export '(READY_FOR_BAG_INFO-VAL
-          READY_FOR_BAG_INFO
-          READY_TO_RECORD-VAL
-          READY_TO_RECORD
-          SAVED_VIDEO-VAL
-          SAVED_VIDEO
-))
