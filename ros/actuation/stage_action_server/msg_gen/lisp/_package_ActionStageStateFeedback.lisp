a66476f296892d1873ae7b175430662f635817f0
Steve Safarik <ssafarik@uw.edu>
2012-03-15 13:55:54 -0700

    Cleaning up git files.

--- ros/actuation/stage_action_server/msg_gen/lisp/_package_ActionStageStateFeedback.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package stage_action_server-msg)
-(cl:export '(STATE-VAL
-          STATE
-))
--- ros/actuation/stage_action_server/msg_gen/lisp/_package_UpdateStagePositionFeedback.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package stage_action_server-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          POSE-VAL
-          POSE
-          VELOCITY-VAL
-          VELOCITY
-))
