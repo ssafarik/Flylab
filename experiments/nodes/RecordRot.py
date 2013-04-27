89d09113bafa50ace20be77fbe4f10a0ca25b7a5
Steve Safarik <ssafarik@uw.edu>
2013-04-26 17:21:02 -0700

    Working on rotcam.

--- experiments/nodes/RecordRot.py
+++ experiments/nodes/RecordRot.py
@@ -27,7 +27,8 @@ class Experiment():
         
         self.experimentparams.save.filenamebase = "rotcam"
         self.experimentparams.save.arenastate = True
-        self.experimentparams.save.video = True
+        self.experimentparams.save.images = False
+        self.experimentparams.save.imagetopic_list = ['camera/image_rect']
         self.experimentparams.save.onlyWhileTriggered = False
         
         self.experimentparams.tracking.exclusionzones.enabled = False
