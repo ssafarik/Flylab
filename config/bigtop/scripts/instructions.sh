a23ab14455a19ef60ed3cc004fd9950755805a6f
Steve Safarik <ssafarik@uw.edu>
2014-09-29 15:13:47 -0700

    Instructions for galvos.

---
+++ config/bigtop/scripts/instructions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo
+echo ==========================================================================
+echo Instructions for operating the galvos:
+echo  1. Turn on illumination.
+echo  2. Turn on the ThorLabs "Laser Diode Controller".
+echo  3. Turn on the Acopian "Galvos Power Supply".
+echo  4. Turn on the "Enable IR Laser" switch.
+echo  5. Verify that all the safety panels \(and their switches\) are closed.  
+echo  6. The laser diode controller "OPEN" LED should be off.
+echo  7. Adjust the laser power as desired.
+echo  8. Run roscore
+echo  9. \(On the Windows galvobox machine\) run galvodriver w/ roscore already running.
+echo 10. roslaunch experiments \<nameofexperiment.py\>
+echo ==========================================================================
+echo
+
